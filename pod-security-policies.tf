cd9bb7f79a95797b7bfa1f143028831f118ae52a
Janosch Schäfer <janosch.schaefer@de.kaercher.com>
2022-04-13 15:29:39 +0200

    always remove default psp

--- pod-security-policies.tf
+++ pod-security-policies.tf
@@ -1,6 +1,4 @@
 resource "null_resource" "remove_default_privileged_policy" {
-  count = var.enable_restricted_security_policy ? 1 : 0
-
   provisioner "local-exec" {
     command = <<EOF
 aws eks update-kubeconfig --name ${aws_eks_cluster.cluster-masters.name}
